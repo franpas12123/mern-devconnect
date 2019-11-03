19444a3280580fb488ca05713d130559ffabe5ea
Frances D Pasulohan <franpas12123@gmail.com>
2019-11-04 02:49:55 +0800

    Connect to MongoDB and defined routes

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 // initialize app with express
 const app = express();
+
+// connect to db
+connectDB();
+
 // test
 app.get('/', (req, res) => res.send('API running'));
+
+// define routes
+// app.use(endpoint, file-containing-the-routes)
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // initialize PORT
 const PORT = process.env.PORT || 5000;
 // listen the app to PORT
